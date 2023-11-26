9ce7c072dd56dabbc0549132adfa341ab6bd7222
Phuc Nguyen <78249185+quatabenho@users.noreply.github.com>
2023-11-26 20:15:17 +0700

    fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,14 +33,14 @@ const Navbar = () => {
 			setAuth({ username: null, email: null, role: null, token: null })
 			sessionStorage.clear()
 			navigate('/')
-			toast.success('Logout successful!', {
+			toast.success('Đăng xuất thành công!', {
 				position: 'top-center',
 				autoClose: 2000,
 				pauseOnHover: false
 			})
 		} catch (error) {
 			console.error(error)
-			toast.error('Error', {
+			toast.error('Lỗi', {
 				position: 'top-center',
 				autoClose: 2000,
 				pauseOnHover: false
@@ -53,95 +53,95 @@ const Navbar = () => {
 	const menuLists = () => {
 		return (
 			<>
-				<div className="flex flex-col gap-2 lg:flex-row">
+				<div className="flex flex-col gap-3 lg:flex-row">
 					<Link
 						to={'/cinema'}
-						className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-white hover:bg-gray-500 ${
+						className={`flex items-center justify-center gap-3 rounded-md px-3 py-3 text-black hover:bg-orange-50 ${
 							window.location.pathname === '/cinema'
-								? 'bg-gradient-to-br from-indigo-800 to-blue-700'
-								: 'bg-gray-600'
+								? 'bg-orange-50'
+								: 'bg-orange-200'
 						}`}
 					>
 						<HomeModernIcon className="h-6 w-6" />
-						<p>Cinema</p>
+						<p>Cụm rạp</p>
 					</Link>
 					<Link
 						to={'/schedule'}
-						className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-white hover:bg-gray-500 ${
+						className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-black hover:bg-orange-50 ${
 							window.location.pathname === '/schedule'
-								? 'bg-gradient-to-br from-indigo-800 to-blue-700'
-								: 'bg-gray-600'
+								? 'bg-orange-50'
+								: 'bg-orange-200'
 						}`}
 					>
 						<ClockIcon className="h-6 w-6" />
-						<p>Schedule</p>
+						<p>Lịch chiếu</p>
 					</Link>
 					{auth.role && (
 						<Link
 							to={'/ticket'}
-							className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-white hover:bg-gray-500 ${
+							className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-black hover:bg-orange-50 ${
 								window.location.pathname === '/ticket'
-									? 'bg-gradient-to-br from-indigo-800 to-blue-700'
-									: 'bg-gray-600'
+									? 'bg-orange-50'
+									: 'bg-orange-200'
 							}`}
 						>
 							<TicketIcon className="h-6 w-6" />
-							<p>Ticket</p>
+							<p>Quản lý vé</p>
 						</Link>
 					)}
 					{auth.role === 'admin' && (
 						<>
 							<Link
 								to={'/movie'}
-								className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-white hover:bg-gray-500 ${
+								className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-black hover:bg-orange-50 ${
 									window.location.pathname === '/movie'
-										? 'bg-gradient-to-br from-indigo-800 to-blue-700'
-										: 'bg-gray-600'
+										? 'bg-orange-50'
+										: 'bg-orange-200'
 								}`}
 							>
 								<VideoCameraIcon className="h-6 w-6" />
-								<p>Movie</p>
+								<p>Danh sách phim</p>
 							</Link>
 							<Link
 								to={'/search'}
-								className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-white hover:bg-gray-500 ${
+								className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-black hover:bg-orange-50 ${
 									window.location.pathname === '/search'
-										? 'bg-gradient-to-br from-indigo-800 to-blue-700'
-										: 'bg-gray-600'
+										? 'bg-orange-50'
+										: 'bg-orange-200'
 								}`}
 							>
 								<MagnifyingGlassIcon className="h-6 w-6" />
-								<p>Search</p>
+								<p>Tìm kiếm</p>
 							</Link>
 							<Link
 								to={'/user'}
-								className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-white hover:bg-gray-500 ${
+								className={`flex items-center justify-center gap-2 rounded-md px-2 py-1 text-black hover:bg-orange-50 ${
 									window.location.pathname === '/user'
-										? 'bg-gradient-to-br from-indigo-800 to-blue-700'
-										: 'bg-gray-600'
+										? 'bg-orange-50'
+										: 'bg-orange-200'
 								}`}
 							>
 								<UsersIcon className="h-6 w-6" />
-								<p>User</p>
+								<p>Quản lý người dùng</p>
 							</Link>
 						</>
 					)}
 				</div>
 				<div className="flex grow items-center justify-center gap-3 lg:justify-end">
 					{auth.username && (
-						<p className="text-md whitespace-nowrap leading-none text-white">Welcome {auth.username}!</p>
+						<p className="text-md whitespace-nowrap leading-none text-white">Xin chào <strong class="text-danger"> {auth.username}</strong></p>
 					)}
 					{auth.token ? (
 						<button
-							className="rounded-lg bg-gradient-to-br from-indigo-600 to-blue-500 px-2 py-1 text-white drop-shadow-md hover:from-indigo-500 hover:to-blue-400 disabled:from-slate-500 disabled:to-slate-400"
+							className="rounded-lg bg-orange-400	 px-2 py-1 text-white drop-shadow-md hover:from-indigo-500 hover:bg-orange-500 disabled:from-slate-500 disabled:to-slate-400"
 							onClick={() => onLogout()}
 							disabled={isLoggingOut}
 						>
-							{isLoggingOut ? 'Processing...' : 'Logout'}
+							{isLoggingOut ? 'Đang xử lý...' : 'Đăng xuất'}	
 						</button>
 					) : (
-						<button className="rounded-lg bg-gradient-to-br from-indigo-600 to-blue-500 px-2 py-1 text-white drop-shadow-md hover:from-indigo-500 hover:to-blue-400">
-							<Link to={'/login'}>Login</Link>
+						<button className="rounded-lg bg-orange-400	 px-2 py-1 text-white drop-shadow-md hover:bg-orange-500">
+							<Link to={'/login'}>Đăng nhập</Link>
 						</button>
 					)}
 				</div>
@@ -150,11 +150,12 @@ const Navbar = () => {
 	}
 
 	return (
-		<nav className="flex flex-col items-center justify-between gap-2 bg-gray-900 px-4 py-3 drop-shadow-lg lg:flex-row lg:justify-start sm:px-8">
+		<nav className="flex flex-col items-center justify-between gap-2 bg-orange-300 px-4 py-3 drop-shadow-lg lg:flex-row lg:justify-start sm:px-8">
 			<div className="flex w-full flex-row justify-between lg:w-fit">
 				<button className="flex flex-row items-center gap-2" onClick={() => navigate('/')}>
-					<FilmIcon className="h-8 w-8 text-white" />
-					<h1 className="mr-2 text-xl text-white">Cinema</h1>
+					{/* <FilmIcon className="h-8 w-8 text-white" /> */}
+					<img src="https://img.icons8.com/external-topaz-kerismaker/48/external-Galaxy-space-topaz-kerismaker.png" alt="" />
+					<h1 className="mr-2 text-xl text-white "><strong className='uppercase'>MIS Cinemas Vietnam</strong></h1>
 				</button>
 				<button
 					className="flex h-8 w-8 items-center justify-center rounded hover:bg-gray-700 lg:hidden"
